42e32f0640cce1677bd15ed962aed2a06c19d146
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-14 12:42:12 +0200

    returning a list of java bean

--- src/main/java/com/fabrice/springrestapibasics/controllers/StudentController.java
+++ src/main/java/com/fabrice/springrestapibasics/controllers/StudentController.java
@@ -4,6 +4,9 @@ import com.fabrice.springrestapibasics.beans.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //@RestController is used to make our class an MVC controller
 //it is a combination of both @Controller and @ResponseBody
 @RestController
@@ -21,5 +24,16 @@ public class StudentController {
     public Student getStudent(){
         Student std = new Student(1,"Fabrice","Niyongabo",44);
         return std;
+    }
+
+    //returning a list(array) of java bean
+    //here the return type of this endpoint will be a list of students List<Student>
+    @GetMapping("/api/v1/students")
+    public List<Student> getAllStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(11,"Kamana","Fidel",30));
+        students.add(new Student(3,"Fabrice","Niyongabo",30));
+        students.add(new Student(4,"James","Bond",18));
+        return students;
     }
 }
