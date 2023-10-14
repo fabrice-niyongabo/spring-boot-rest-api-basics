cb0fb1ddd0865111e35d2e63ddbb42f9755c53f4
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-14 12:09:54 +0200

    getters and setters for the student class

--- src/main/java/com/fabrice/springrestapibasics/beans/Student.java
+++ src/main/java/com/fabrice/springrestapibasics/beans/Student.java
@@ -5,4 +5,46 @@ public class Student {
     private String fName;
     private  String lName;
     private int Age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", Age=" + Age +
+                '}';
+    }
 }
